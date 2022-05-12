6dadd3f5905cc2177fec418d95307f303ba38669
ciberusps <ciberus.ps@gmail.com>
2022-05-12 22:03:37 +0300

    [wip] youque

--- you-que/you-que.user.js
+++ you-que/you-que.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        YouQue
-// @version     2
+// @version     3
 // @namespace   https://github.com/Ciberusps/user-scripts
 // @updateURL   https://github.com/Ciberusps/user-scripts/raw/main/you-que/you-que.user.js
 // @downloadURL https://github.com/Ciberusps/user-scripts/raw/main/you-que/you-que.user.js
@@ -24,72 +24,136 @@
 // ==/UserScript==
 
 (function () {
-  const debug = true;
+  const IS_DEV = true;
+
+  let sidebarRootEl = null;
+  let sidebarEl = null;
+
+  function createSidebarEl() {
+    sidebarEl = document.createElement("div");
+    // sidebarEl.id = "you-que-sidebar";
+    sidebarEl.style.display = "flex";
+    sidebarEl.style.flexDirection = "column";
+    sidebarEl.style.alignItems = "center";
+    sidebarEl.style.width = "400px";
+    sidebarEl.style.color = "white";
+    sidebarEl.style.fontSize = "16px";
+    sidebarEl.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
+
+    for (let i = 0; i < 10; i++) {
+      const testVideo = document.createElement("div");
+      testVideo.style.display = "flex";
+      // testVideo.style.flexDirection = "column";
+      testVideo.style.alignItems = "center";
+      testVideo.style.width = "100%";
+      testVideo.style.height = "50px";
+      testVideo.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+      testVideo.style.marginBottom = "5px";
+      testVideo.textContent = "Video " + i;
+      sidebarEl.appendChild(testVideo);
+    }
+  }
+
+  function insertSidebar() {
+    // Select the node that will be observed for mutations
+    sidebarRootEl = document.getElementById("page-manager");
+    debugLog("sidebarRootEl");
+    debugLog(sidebarRootEl);
+    if (!sidebarRootEl) throw new Error("Failed to insert sidebar");
+
+    // Callback function to execute when mutations are observed
+    const callback = function (mutationsList, observer) {
+      // Use traditional 'for loops' for IE 11
+      for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          debugLog("A child node has been added or removed.");
+
+          createSidebarEl();
+          sidebarRootEl.appendChild(sidebarEl);
+
+          observer.disconnect();
+        }
+      }
+    };
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    // Start observing the target node for configured mutations
+    observer.observe(sidebarRootEl, { childList: true });
+  }
 
   async function onLoaded() {
-    debug && console.log("YouQue loaded");
-    const pathname = window.location.pathname;
-    const parts = pathname.split("/").filter((s) => s.length > 0);
-    const gameId = Number(parts[1]);
-    debug && console.log(parts, parts.length);
-    debug && console.log({ gameId });
+    debugLog("YouQue loaded");
+    // style-scope ytd-playlist-video-list-renderer
 
-    const isValidGamePath = parts.length === 3 && !Number.isNaN(gameId);
-    if (!isValidGamePath) return null;
+    insertSidebar();
 
-    const steamSpyData = await request({
-      method: "GET",
-      url: `https://steamspy.com/api.php?request=appdetails&appid=${gameId}`,
-    });
+    // const pathname = window.location.pathname;
+    // const parts = pathname.split("/").filter((s) => s.length > 0);
+    // const gameId = Number(parts[1]);
+    // debug && console.log(parts, parts.length);
+    // debug && console.log({ gameId });
 
-    debug && console.log(steamSpyData);
-    // console.log({ response2: res2.response    });
-    // const json = await res2.json();
-    // console.log({ json })
-    if (!pricesRes) return null;
+    // const isValidGamePath = parts.length === 3 && !Number.isNaN(gameId);
+    // if (!isValidGamePath) return null;
 
-    const price = getPrice(pricesRes);
-    if (!price) return null;
-    debug && console.log("PRICE", price);
+    // const steamSpyData = await request({
+    //   method: "GET",
+    //   url: `https://steamspy.com/api.php?request=appdetails&appid=${gameId}`,
+    // });
 
-    const reviewsCount = Number(
-      document.querySelector('meta[itemprop="reviewCount"]').getAttribute("content")
-    );
-    debug && console.log("REVIEW COUNT", reviewsCount);
+    // debug && console.log(steamSpyData);
+    // // console.log({ response2: res2.response    });
+    // // const json = await res2.json();
+    // // console.log({ json })
+    // if (!pricesRes) return null;
 
-    const profit = { gross: reviewsCount * 60 * price };
-    profit.net = profit.gross * (1 - STEAM_CUT);
-    profit.cut = profit.gross * STEAM_CUT;
-    debug && console.log(profit);
+    // const price = getPrice(pricesRes);
+    // if (!price) return null;
+    // debug && console.log("PRICE", price);
 
-    var formatter = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      maximumFractionDigits: 0,
-    });
+    // const reviewsCount = Number(
+    //   document.querySelector('meta[itemprop="reviewCount"]').getAttribute("content")
+    // );
+    // debug && console.log("REVIEW COUNT", reviewsCount);
 
-    const imgContainerEl = document.querySelector("div.game_header_image_ctn");
-    const netProfitEl = document.createElement("div");
-    netProfitEl.style.background = "#000B";
-    netProfitEl.style.padding = "5px 10px";
-    netProfitEl.style.position = "absolute";
-    netProfitEl.style.bottom = "0px";
-    netProfitEl.style.right = "0px";
-    netProfitEl.innerText =
-      "Net: " +
-      formatter.format(profit.net) +
-      " | Gross: " +
-      formatter.format(profit.gross) +
-      " | Steam Cut: " +
-      formatter.format(profit.cut);
+    // const profit = { gross: reviewsCount * 60 * price };
+    // profit.net = profit.gross * (1 - STEAM_CUT);
+    // profit.cut = profit.gross * STEAM_CUT;
+    // debug && console.log(profit);
 
-    imgContainerEl.style.position = "relative";
-    imgContainerEl.appendChild(netProfitEl);
-    debug && console.log("INJECTED");
+    // var formatter = new Intl.NumberFormat("en-US", {
+    //   style: "currency",
+    //   currency: "USD",
+    //   maximumFractionDigits: 0,
+    // });
+
+    // const imgContainerEl = document.querySelector("div.game_header_image_ctn");
+    // const netProfitEl = document.createElement("div");
+    // netProfitEl.style.background = "#000B";
+    // netProfitEl.style.padding = "5px 10px";
+    // netProfitEl.style.position = "absolute";
+    // netProfitEl.style.bottom = "0px";
+    // netProfitEl.style.right = "0px";
+    // netProfitEl.innerText =
+    //   "Net: " +
+    //   formatter.format(profit.net) +
+    //   " | Gross: " +
+    //   formatter.format(profit.gross) +
+    //   " | Steam Cut: " +
+    //   formatter.format(profit.cut);
+
+    // imgContainerEl.style.position = "relative";
+    // imgContainerEl.appendChild(netProfitEl);
+    debugLog("INJECTED");
   }
 
   if (!window.__YOU_QUE_LOADED) {
     window.__YOU_QUE_LOADED = true;
     window.addEventListener("load", onLoaded);
   }
+
+  debugLog = function (...args) {
+    IS_DEV && console.log(...args);
+  };
 })();
