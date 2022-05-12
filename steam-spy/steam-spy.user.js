029dc75224d0e3e662071cf7da7c24d0b7715092
ciberusps <ciberus.ps@gmail.com>
2022-05-12 19:21:29 +0300

    steam spy fix loading from local

--- steam-spy/steam-spy.user.js
+++ steam-spy/steam-spy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Steam spy
-// @version     5
+// @version     8
 // @namespace   https://github.com/Ciberusps/user-scripts
 // @updateURL   https://github.com/Ciberusps/user-scripts/raw/main/steam-spy/steam-spy.user.js
 // @downloadURL https://github.com/Ciberusps/user-scripts/raw/main/steam-spy/steam-spy.user.js
@@ -8,79 +8,93 @@
 // @author      CiberusPS
 // @match       *://*store.steampowered.com/*
 // @grant       GM.xmlHttpRequest
-// @connect     *://*steamspy.com/*
+// @connect     steamspy.com
 // @require     file://F:\user-scripts\steam-spy\steam-spy.user.js
 // ==/UserScript==
 
+(function () {
+  const debug = true;
 
-function getPrice(data) {
-  try {
-    const firstKey = Object.keys(data)[0];
-    return (data[firstKey].data.price.initial / 100) * AVG_PRICE_MODIFIER;
-  } catch (err) {
-    return null;
+  function getPrice(data) {
+    try {
+      const firstKey = Object.keys(data)[0];
+      return (data[firstKey].data.price.initial / 100) * AVG_PRICE_MODIFIER;
+    } catch (err) {
+      return null;
+    }
   }
-}
 
-async function onLoaded() {
-  debug && console.log("LOADED EVENT", window.location.href);
-  const pathname = window.location.pathname;
-  const parts = pathname.split("/").filter((s) => s.length > 0);
-  const gameId = Number(parts[1]);
-  debug && console.log(parts, parts.length);
-  debug && console.log({ gameId })
+  async function request(options) {
+    try {
+      const res2 = await GM.xmlHttpRequest(options);
+      return res2.json();
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-  const isValidGamePath = parts.length === 3 && !Number.isNaN(gameId);
-  if (!isValidGamePath) return null;
+  async function onLoaded() {
+    console.log("KOJPOIJPOJPNOIJ");
+    debug && console.log("LOADED EVENT v7", window.location.href);
+    const pathname = window.location.pathname;
+    const parts = pathname.split("/").filter((s) => s.length > 0);
+    const gameId = Number(parts[1]);
+    debug && console.log(parts, parts.length);
+    debug && console.log({ gameId });
 
-  
-  const res2 = await GM.xmlHttpRequest({ method: "GET", url: `https://steamspy.com/api.php?request=appdetails&appid=${gameId}` });
-  
-  debug && console.log({ response: res2  });
-  const json = await res2.json();
-  console.log({ json })
-  if (!pricesRes) return null;
+    const isValidGamePath = parts.length === 3 && !Number.isNaN(gameId);
+    if (!isValidGamePath) return null;
 
-  const price = getPrice(pricesRes);
-  if (!price) return null;
-  debug && console.log("PRICE", price);
+    const res = await request({
+      method: "GET",
+      url: `https://steamspy.com/api.php?request=appdetails&appid=${gameId}`,
+    });
 
-  const reviewsCount = Number(
-    document
-      .querySelector('meta[itemprop="reviewCount"]')
-      .getAttribute("content")
-  );
-  debug && console.log("REVIEW COUNT", reviewsCount);
+    debug && console.log({ response: res });
+    // console.log({ response2: res2.response    });
+    // const json = await res2.json();
+    // console.log({ json })
+    if (!pricesRes) return null;
 
-  const profit = { gross: reviewsCount * 60 * price };
-  profit.net = profit.gross * (1 - STEAM_CUT);
-  profit.cut = profit.gross * STEAM_CUT;
-  debug && console.log(profit);
+    const price = getPrice(pricesRes);
+    if (!price) return null;
+    debug && console.log("PRICE", price);
 
-  var formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    maximumFractionDigits: 0,
-  });
+    const reviewsCount = Number(
+      document.querySelector('meta[itemprop="reviewCount"]').getAttribute("content")
+    );
+    debug && console.log("REVIEW COUNT", reviewsCount);
 
-  const imgContainerEl = document.querySelector("div.game_header_image_ctn");
-  const netProfitEl = document.createElement("div");
-  netProfitEl.style.background = "#000B";
-  netProfitEl.style.padding = "5px 10px";
-  netProfitEl.style.position = "absolute";
-  netProfitEl.style.bottom = "0px";
-  netProfitEl.style.right = "0px";
-  netProfitEl.innerText =
-    "Net: " +
-    formatter.format(profit.net) +
-    " | Gross: " +
-    formatter.format(profit.gross) +
-    " | Steam Cut: " +
-    formatter.format(profit.cut);
+    const profit = { gross: reviewsCount * 60 * price };
+    profit.net = profit.gross * (1 - STEAM_CUT);
+    profit.cut = profit.gross * STEAM_CUT;
+    debug && console.log(profit);
 
-  imgContainerEl.style.position = "relative";
-  imgContainerEl.appendChild(netProfitEl);
-  debug && console.log("INJECTED");
-}
+    var formatter = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      maximumFractionDigits: 0,
+    });
 
-window.addEventListener("load", onLoaded);
+    const imgContainerEl = document.querySelector("div.game_header_image_ctn");
+    const netProfitEl = document.createElement("div");
+    netProfitEl.style.background = "#000B";
+    netProfitEl.style.padding = "5px 10px";
+    netProfitEl.style.position = "absolute";
+    netProfitEl.style.bottom = "0px";
+    netProfitEl.style.right = "0px";
+    netProfitEl.innerText =
+      "Net: " +
+      formatter.format(profit.net) +
+      " | Gross: " +
+      formatter.format(profit.gross) +
+      " | Steam Cut: " +
+      formatter.format(profit.cut);
+
+    imgContainerEl.style.position = "relative";
+    imgContainerEl.appendChild(netProfitEl);
+    debug && console.log("INJECTED");
+  }
+
+  window.addEventListener("load", onLoaded);
+})();
